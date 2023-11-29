33d81d4be74ad9909c61ca5e0e812b2b1c1458d9
mambasa <101706131+kal990574@users.noreply.github.com>
2023-11-30 01:11:49 +0900

    feat: 3d transformation

--- Viewing/Viewing/main.cpp
+++ Viewing/Viewing/main.cpp
@@ -1,10 +1,11 @@
-// #define GLEW_STATIC
-// #include "GL/glew.h"
 #define GL_SILENCE_DEPRECATION
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <vector>
 #include <iostream>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/transform.hpp>
+#include "j3a.hpp"
 #include "toys.h"
 using namespace glm;
 using namespace std;
@@ -17,6 +18,23 @@ void init();
 GLuint vertexBuffer = 0;
 GLuint indexBuffer = 0;
 GLuint vertexArray = 0;
+float theta = 0;
+float phi = 0;
+double lastX = 0, lastY = 0;
+
+void cursorCB(GLFWwindow* w, double xPos, double yPos){
+    if(glfwGetMouseButton(w, GLFW_MOUSE_BUTTON_1)){
+        theta -= (xPos - lastX)/300;
+        phi -= (yPos - lastY)/300;
+        lastX = xPos;
+        lastY = yPos;
+    }
+}
+void mouseBtnCB(GLFWwindow* w, int button, int action, int mods){
+    if( button == GLFW_MOUSE_BUTTON_1 && action == GLFW_PRESS){
+        glfwGetCursorPos(w, &lastX, &lastY);
+    }
+}
 int main(){
     if(!glfwInit()) return 0;
     
@@ -28,8 +46,9 @@ glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
 #endif
     
     GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", 0, 0);
+    glfwSetCursorPosCallback(window, cursorCB);
+    glfwSetMouseButtonCallback(window, mouseBtnCB);
     glfwMakeContextCurrent(window);
-    //glewInit();
     init();
     while(!glfwWindowShouldClose(window)){
         render(window);
@@ -39,18 +58,17 @@ glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
     glfwTerminate();
     return 0;
 }
+
 void init(){
+    loadJ3A( "bunny.j3a" );
     program.loadShaders("shader.vert", "shader.frag");
-    vector<vec3> p = {{0.5,-0.5,0}, {0,0.5,0}, {-0.5,-0.5,0}, {0,-0.5,0}, {-0.3,0,0}, {0.3,0,0}};
-    vector<uvec3> triangles = {{0,3,5}, {2,4,3}, {1,4,5}};
-    
     glGenBuffers(1, &vertexBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, p.size()*sizeof(vec3), p.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, nVertices[0]*sizeof(glm::vec3), vertices[0], GL_STATIC_DRAW);
     
     glGenBuffers(1, &indexBuffer);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, triangles.size()*sizeof(uvec3), triangles.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, nTriangles[0]*sizeof(glm::u32vec3), triangles[0], GL_STATIC_DRAW);
     
     glGenVertexArrays(1, &vertexArray);
     glBindVertexArray(vertexArray);
@@ -63,12 +81,24 @@ void render( GLFWwindow* window){
     int w,h;
     glfwGetFramebufferSize(window, &w, &h);
     glViewport(0,0,w,h);
+    vec3 cameraPosition = vec3(0,0,7);
+    cameraPosition = vec3(rotate(theta, vec3(0,1,0)) * rotate(phi, vec3(1,0,0)) * vec4(cameraPosition,1));
+    
+    mat4 projMat = perspective(radians(45.f), w/float(h), 0.1f, 100.f);
+    mat4 viewMat = lookAt(cameraPosition, vec3(0), vec3(0,1,0));
+    mat4 modelMat = mat4(1);
+    
     glClearColor(0,0,0.7,0);
     glClear(GL_COLOR_BUFFER_BIT);
     glUseProgram(program.programID);
+    GLuint loc = glGetUniformLocation(program.programID, "modelMat");
+    glUniformMatrix4fv(loc, 1, false, value_ptr(modelMat));
+    loc = glGetUniformLocation(program.programID, "viewMat");
+    glUniformMatrix4fv(loc, 1, false, value_ptr(viewMat));
+    loc = glGetUniformLocation(program.programID, "projMat");
+    glUniformMatrix4fv(loc, 1, false, value_ptr(projMat));
     glBindVertexArray(vertexArray);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
-    // 몇번째 점까지
-    glDrawElements(GL_TRIANGLES, 3*3, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, nTriangles[0]*3, GL_UNSIGNED_INT, 0);
     glfwSwapBuffers(window);
 }

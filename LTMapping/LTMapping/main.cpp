c99607a8c0010bc88cbbdb3d29260ef7106ea5f2
mambasa <101706131+kal990574@users.noreply.github.com>
2023-12-18 12:18:12 +0900

    init: LTMapping project

---
+++ LTMapping/LTMapping/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LTMapping
+//
+//  Created by 이재현 on 2023/12/18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
